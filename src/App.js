64b7822ca23409f49a9a9d05d75e750cb4f98df0
nitinpathak337 <nitinpathak337@gmail.com>
2023-03-05 11:44:53 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { MathJax } from "better-react-mathjax";
+import { Bars } from "react-loader-spinner";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+const totalQuestionsArr = [1, 2, 3];
 
 function App() {
+  const [questionNo, setQuestionNo] = useState(1);
+  const [questionData, setQuestionData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [questionNo]);
+
+  const getData = async () => {
+    let questionID = null;
+    setQuestionData([]);
+    switch (questionNo) {
+      case 1:
+        questionID = "AreaUnderTheCurve_901";
+        break;
+      case 2:
+        questionID = "BinomialTheorem_901";
+        break;
+      case 3:
+        questionID = "DifferentialCalculus2_901";
+        break;
+      default:
+        break;
+    }
+    const response = await fetch(
+      `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${questionID}`
+    );
+    const data = await response.json();
+    setQuestionData(data[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Nioclass Quiz</h1>
+
+      <p className="question-para">{`Question ${questionNo}/3`}</p>
+      {questionData.length === 0 ? (
+        <div className="loader-container">
+          <Bars color="chocolate" />
+        </div>
+      ) : (
+        <MathJax>
+          <p className="chapter">{`Chapter : ${questionData.ChapterID}`}</p>
+          <p className=" question">{`Q. ${questionData.Question}`}</p>
+        </MathJax>
+      )}
+
+      <div className="btn-container">
+        {questionNo > 1 ? (
+          <button type="button" onClick={() => setQuestionNo(questionNo - 1)}>
+            <GrPrevious />
+          </button>
+        ) : (
+          <button type="button" disabled className="disabled">
+            <GrPrevious />
+          </button>
+        )}
+        <ul className="list-container">
+          {totalQuestionsArr.map((eachItem) => {
+            const activeBtn = eachItem === questionNo ? "active-btn" : "";
+
+            return (
+              <li key={eachItem}>
+                <button
+                  type="button"
+                  onClick={() => setQuestionNo(eachItem)}
+                  className={`${activeBtn}`}
+                >
+                  {eachItem}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        {questionNo < 3 ? (
+          <button type="button" onClick={() => setQuestionNo(questionNo + 1)}>
+            <GrNext />
+          </button>
+        ) : (
+          <button type="button" disabled className="disabled">
+            <GrNext />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MathJaxContext } from "better-react-mathjax";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MathJaxContext>
+      <App />
+    </MathJaxContext>
   </React.StrictMode>
 );
 
